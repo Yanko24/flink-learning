6e0e890a36ab7a07ea7aa4045599fd4dac4f3a06
Yankee <yangqi199808@gmail.com>
2021-10-14 17:21:35 +0800

    :art: 测试类demo

--- demo/src/main/java/Sink2Kafka.java
+++ demo/src/main/java/Sink2Kafka.java
@@ -1,9 +1,8 @@
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 
 import java.util.Properties;
@@ -16,14 +15,14 @@ import java.util.Properties;
  */
 public class Sink2Kafka {
     /**
-     * source-kafka地址
+     * source-kafka地址：198.98.20.202:9092,198.98.20.205:9092,198.98.20.207:9092
      */
-    private static final String SOURCE_BOOTSTRAP_SERVERS = "hadoop01:9092,hadoop02:9092,hadoop03:9092";
+    private static final String SOURCE_BOOTSTRAP_SERVERS = "198.98.31.80:9092,198.98.31.81:9092,198.98.31.82:9092";
 
     /**
-     * sink-kafka地址
+     * sink-kafka地址：198.98.31.80:9092,198.98.31.81:9092,198.98.31.82:9092
      */
-    private static final String SINK_BOOTSTRAP_SERVERS = "hadoop01:9092,hadoop02:9092,hadoop03:9092";
+    private static final String SINK_BOOTSTRAP_SERVERS = "198.98.31.80:9092,198.98.31.81:9092,198.98.31.82:9092";
 
     /**
      * 消费者组
@@ -33,12 +32,12 @@ public class Sink2Kafka {
     /**
      * source-topic
      */
-    private static final String SOURCE_TOPIC = "bill_change_topic";
+    private static final String SOURCE_TOPIC = "test_ogg";
 
     /**
      * sink-topic
      */
-    private static final String SINK_TOPIC = "bill_change_topic_2";
+    private static final String SINK_TOPIC = "flink_ogg_test";
 
     public static void main(String[] args) throws Exception {
         // 引入流执行环境
@@ -52,7 +51,7 @@ public class Sink2Kafka {
         propertiesSource.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
 
         // 定义消费者
-        FlinkKafkaConsumer010<String> sourceConsumer = new FlinkKafkaConsumer011<>(SOURCE_TOPIC, new SimpleStringSchema(), propertiesSource);
+        FlinkKafkaConsumer<String> sourceConsumer = new FlinkKafkaConsumer<>(SOURCE_TOPIC, new SimpleStringSchema(), propertiesSource);
         DataStreamSource<String> source = env.addSource(sourceConsumer);
 
         // 打印源端消费数据
@@ -63,7 +62,7 @@ public class Sink2Kafka {
         propertiesSink.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, SINK_BOOTSTRAP_SERVERS);
 
         // 定义生产者
-        FlinkKafkaProducer011<String> sinkProducer = new FlinkKafkaProducer011<>(SINK_TOPIC, new SimpleStringSchema(), propertiesSink);
+        FlinkKafkaProducer<String> sinkProducer = new FlinkKafkaProducer<>(SINK_TOPIC, new SimpleStringSchema(), propertiesSink);
         source.addSink(sinkProducer);
 
         // 提交作业
