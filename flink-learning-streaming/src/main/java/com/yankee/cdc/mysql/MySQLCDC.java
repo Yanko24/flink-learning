e9852355b5f136ec1f87a1f309098f9029c2a8af
Yankee <yangqi199808@gmail.com>
2022-01-11 10:58:26 +0800

    🎨 修改项目架构

---
+++ flink-learning-streaming/src/main/java/com/yankee/cdc/mysql/MySQLCDC.java
@@ -0,0 +1,102 @@
+package com.yankee.cdc.mysql;
+
+import com.alibaba.fastjson.JSONObject;
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.debezium.DebeziumDeserializationSchema;
+import io.debezium.data.Envelope;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+/**
+ * @author Yankee
+ * @version 1.0
+ * @description TODO
+ * @date 2022/1/10 20:13
+ */
+public class MySQLCDC {
+    private static final Logger LOG = LoggerFactory.getLogger(MySQLCDC.class);
+
+    public static void main(String[] args) throws Exception {
+        Properties debeziumProperties = new Properties();
+        debeziumProperties.put("snapshot.locking.mode", "none");
+        // MySQLCDC
+        MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
+                .hostname("localhost")
+                .port(3306)
+                .databaseList("demo")
+                .tableList("demo.person")
+                .username("yankee")
+                .password("xiaoer")
+                // .startupOptions(StartupOptions.earliest())
+                .deserializer(new DebeziumDeserializationSchema<String>() {
+                    // 自定义数据源解析器
+                    @Override
+                    public void deserialize(SourceRecord sourceRecord, Collector<String> collector) throws Exception {
+                        // 获取主题信息，包含着数据库库和表名
+                        String topic = sourceRecord.topic();
+                        String[] arr = topic.split("\\.");
+                        String db = arr[1];
+                        String tableName = arr[2];
+
+                        // 获取操作类型：READ DELETE UPDATE CREATE
+                        Envelope.Operation operation = Envelope.operationFor(sourceRecord);
+
+                        // 获取值信息并转换为struct类型
+                        Struct value = (Struct) sourceRecord.value();
+
+                        // 获取变化后的数据
+                        Struct after = value.getStruct("after");
+
+                        // 创建JSON用于存储数据信息
+                        JSONObject data = new JSONObject();
+                        if (after != null) {
+                            Schema schema = after.schema();
+                            for (Field field : schema.fields()) {
+                                data.put(field.name(), after.get(field.name()));
+                            }
+                        }
+
+                        // 创建JSON对象用于封装最终返回值数据信息
+                        JSONObject result = new JSONObject();
+                        result.put("operation", operation.toString().toLowerCase());
+                        result.put("data", data);
+                        result.put("database", db);
+                        result.put("table", tableName);
+
+                        // 发送数据到下游
+                        collector.collect(result.toJSONString());
+                    }
+
+                    @Override
+                    public TypeInformation<String> getProducedType() {
+                        return TypeInformation.of(String.class);
+                    }
+                })
+                .debeziumProperties(debeziumProperties)
+                .build();
+
+        // 获取流执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // 设置checkpoint
+        env.enableCheckpointing(30000, CheckpointingMode.EXACTLY_ONCE);
+
+        env
+                .fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
+                .print()
+                .setParallelism(1);
+
+        // 提交执行
+        env.execute();
+    }
+}
