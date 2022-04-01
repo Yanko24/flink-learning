df37734440c8ee26c20f5667b2c3bd8c03e146d8
Yankee <yangqi199808@gmail.com>
2022-04-01 19:19:18 +0800

    ⚡️: flink-cdc(mysql+postgrew-es)

---
+++ flink-learning-cdc/mysql-postgres-es/src/main/scala/com/yankee/cdc/stream/MysqlAndPostgresToElasticsearch.scala
@@ -0,0 +1,12 @@
+package com.yankee.cdc.stream
+
+/**
+ * @Description flink-cdc(mysql+postgres-es)
+ * @Date 2022/4/1 19:17
+ * @Author yankee
+ */
+object MysqlAndPostgresToElasticsearch {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
